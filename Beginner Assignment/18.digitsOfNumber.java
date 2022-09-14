bac539d92ee48a9509951e45484b66e6e6275e48
Vanitha-A-hub <vanithameena582@gmail.com>
2022-09-14 19:43:27 +0530

    Assignments are done

--- Beginner Assignment/18.digitsOfNumber.java
+++ Beginner Assignment/18.digitsOfNumber.java
@@ -3,8 +3,12 @@ class Main {
   public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
     int n=sc.nextInt();
+    int freq[]=new int[10];
     while(n>0){
-      System.out.print(n%10+" ");
+      if(freq[n%10]==0){
+        System.out.print(n%10+" ");
+        freq[n%10]=1;
+      }
       n/=10;
     }
     sc.close();
